98d01c3e925b052a340620a2946fb261a5706484
75htrx <vishnu.navaneetham@outlook.com>
2025-09-03 14:45:01 +0530

    Fix user name fallback when Clerk first_name and last_name are null

--- inngest/index.js
+++ inngest/index.js
@@ -14,11 +14,14 @@ const SyncUserCreation = inngest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } = event.data;
 
     const userData = {
-      _id: id,
-      email: email_addresses[0].email_address,
-      name: `${first_name} ${last_name}`,
-      image: image_url,
-    };
+  _id: id,
+  email: email_addresses[0].email_address,
+  name: (first_name && last_name) 
+    ? `${first_name} ${last_name}` 
+    : email_addresses[0].email_address.split("@")[0], // fallback
+  image: image_url,
+};
+
 
     await User.create(userData);
   }
