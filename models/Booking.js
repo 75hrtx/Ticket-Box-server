1330aa93d94648fc56ae6b92b163f7ef6d89fe8f
75htrx <vishnu.navaneetham@outlook.com>
2025-09-03 16:34:54 +0530

    fix

--- models/Booking.js
+++ models/Booking.js
@@ -1,16 +1,14 @@
+// server/models/Booking.js
 import mongoose from "mongoose";
 
 const bookingSchema = new mongoose.Schema({
   movieId: { type: String, required: true },
-  movieTitle: { type: String, required: true },
-  movieImage: { type: String },
-  userId: { type: String, required: true },
-  seats: { type: [String], required: true },
   date: { type: String, required: true },
   time: { type: String, required: true },
-  price: { type: Number, required: true },
-  status: { type: String, default: "reserved" },
-  expiresAt: { type: Date },
-});
+  seats: { type: [String], required: true },
+  userId: { type: String, required: true }
+}, { timestamps: true });
 
-export default mongoose.model("Booking", bookingSchema);
+const Booking = mongoose.model("Booking", bookingSchema);
+
+export default Booking;
